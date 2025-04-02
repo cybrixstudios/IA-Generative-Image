b6db75a1c81afcfa26ad1fc39786b9aed008341e
cybrixstudios <yorshalvarez@cybrixstudios.com>
2025-04-01 23:34:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -40,5 +40,6 @@ def generar():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
